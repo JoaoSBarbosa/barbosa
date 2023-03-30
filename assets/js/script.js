33ab82314ec02be4fc44623484722392e35d3f31
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-30 20:43:05 -0300

    Tratamento de acessibilidade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-const btnMobile = document.getElementById('btnMobile');
+const btnMobile = document.getElementById("btnMobile");
 
-function toggleMenu(){
-   const nav = document.querySelector(".menu");
-   nav.classList.toggle('ativo');
+function toggleMenu(e) {
+  if (e.type === "touchstart") e.preventDefault();
+
+  const nav = document.querySelector(".menu");
+  nav.classList.toggle("ativo");
+  const active = nav.classList.contains("ativo");
+  e.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    e.currentTarget.setAttribute("aria-label", "Fechar menu");
+  } else {
+    e.currentTarget.setAttribute("aria-label", "Abrir menu");
+  }
 }
-btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
