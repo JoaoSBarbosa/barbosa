6ec22541ef4a8da3efd63dac7a44192395233e9d
João Uninter <contato.jsbarbosa@gmail.com>
2023-03-31 22:19:39 -0300

    Efeito de maquina de escrever na seção curriculum

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
-const btnMobile = document.getElementById("btnMobile");
+// Menu Mobile
+function initMobileMenu() {
+  const btnMobile = document.getElementById("btnMobile");
 
-function toggleMenu(e) {
-  if (e.type === "touchstart") e.preventDefault();
+  function toggleMenu(e) {
+    if (e.type === "touchstart") e.preventDefault();
 
-  const nav = document.querySelector(".menu");
-  nav.classList.toggle("ativo");
-  const active = nav.classList.contains("ativo");
-  e.currentTarget.setAttribute("aria-expanded", active);
-  if (active) {
-    e.currentTarget.setAttribute("aria-label", "Fechar menu");
-  } else {
-    e.currentTarget.setAttribute("aria-label", "Abrir menu");
+    const nav = document.querySelector(".menu");
+    nav.classList.toggle("ativo");
+    const active = nav.classList.contains("ativo");
+    e.currentTarget.setAttribute("aria-expanded", active);
+    if (active) {
+      e.currentTarget.setAttribute("aria-label", "Fechar menu");
+    } else {
+      e.currentTarget.setAttribute("aria-label", "Abrir menu");
+    }
   }
+  btnMobile.addEventListener("click", toggleMenu);
+  btnMobile.addEventListener("touchstart", toggleMenu);
 }
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
+initMobileMenu();
+
+// Efeito maquina de escrever
+function activeLetter(e) {
+  const arrayLetter = e.innerHTML.split("");
+  e.innerHTML = "";
+  arrayLetter.forEach((letter, index) => {
+    setTimeout(() => {
+      e.innerHTML += letter;
+    }, 75 * index);
+  });
+}
+
+const devTitle = document.querySelector(".typing");
+activeLetter(devTitle);
