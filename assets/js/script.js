edf1453e139c11d051ccf53afb71608241b63435
João Uninter <contato.jsbarbosa@gmail.com>
2023-04-01 00:14:44 -0300

    Seção Sobre mim

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,25 @@ function activeLetter(e) {
 
 const devTitle = document.querySelector(".typing");
 activeLetter(devTitle);
+
+// Scroll efeito
+
+function initAnimationScrool() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimationScrool();
